b9b8be1a234bc467934af6b8eda7807927a0385f
Kleber <klebersall@gmail.com>
2023-12-07 17:13:41 -0300

    Adicionando os metodos delete e get movie

--- bun-movies-api/src/db.ts
+++ bun-movies-api/src/db.ts
@@ -22,19 +22,18 @@ export class MoviesDatabase {
       return this.db.query('SELECT * FROM movies').all();
   }
 
-  // Add a movie
-  async addBook(movie: Movie) {
-      // q: Get id type safely
-      return this.db.query(`INSERT INTO movies (name, author) VALUES (?, ?) RETURNING id`).get(movie.name, movie.author) as Movie;
+  // Delete a movie
+  async deleteMovie(id: number) {
+      return this.db.run(`DELETE FROM movies WHERE id = ${id}`)
   }
 
-  // Update a movie
-  async updateBook(id: number, movie: Movie) {
-      return this.db.run(`UPDATE movies SET name = '${movie.name}', author = '${movie.author}' WHERE id = ${id}`)
+  async getMovie(id: number) {
+    return this.db.query(`SELECT * FROM movies WHERE id=${id}`).get();
   }
 
   // Initialize the database
   async init() {
     return this.db.run('CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, author TEXT)');
   }
+
 }
